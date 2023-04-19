6fe64296ba87236668cc049338fb47517913764b
laurencelawrencearcher <laurence.lawrence@odysseusinc.com>
2023-04-19 22:22:35 +0200

    xlab fix

--- R/plots.R
+++ R/plots.R
@@ -190,7 +190,7 @@ plotOutput <- function(output,
           panel.background = eb, panel.border = eb,
           axis.ticks.y = eb, axis.text.y = eb, axis.title.y = eb,
           axis.line.x = element_line(color = 'black'), legend.key.size = unit(2, 'cm'))
-  + xlab("Time (Days)")
+  + ggplot2::xlab(label = "Time (Days)")
 
   return(p1)
 
