07ae2839b161706c5936509d09734dae0af9f823
laurencelawrencearcher <laurence.lawrence@odysseusinc.com>
2023-07-18 13:44:57 +0100

    Remove wayward print statements
    Remove onLoad test
    
    Update readme
    
    Update NAMESPACE for input/output
    
    Update RBuildIgnore

--- R/zzz.R
+++ R/zzz.R
@@ -5,7 +5,4 @@
   reticulate::source_python(system.file("./python/score.py",package="oncoRegimens"),envir=globalenv())
   reticulate::source_python(system.file("./python/align.py",package="oncoRegimens"),envir=globalenv())
   reticulate::source_python(system.file("./python/main.py",package="oncoRegimens"),envir=globalenv())
-
-  print("Sourcing python files...")
-
 }
