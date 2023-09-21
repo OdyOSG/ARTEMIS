5e50e76f0919d9968cc56c879be0f5f5e4f14384
laurencelawrencearcher <laurence.lawrence@odysseusinc.com>
2023-09-21 12:13:45 +0200

    Update Data for test

---
+++ R/data.R
@@ -0,0 +1,19 @@
+#' Default Regimen data for lung cancer
+#'
+#' A set of default regimens, derived from HemOnc - Thoracic Cancer
+"regimens"
+
+#' A default set of valid drugs used for alignment
+#'
+#' A set of default valid drugs, derived from HemOnc - Thoracic Cancer
+"validDrugs"
+
+#' A default set of regimen groups, such as chemotherapy, ICI-combinations etc.
+#'
+#' A set of default regimen groups
+"regimen_groups"
+
+#' A default cohort indicating metastatic lung cancer patients
+#'
+#' A default cohort json
+"json"
