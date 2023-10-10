73d00ba33c9d08895f16fb83089c520f4ccd776e
laurencelawrencearcher <laurence.lawrence@odysseusinc.com>
2023-10-10 08:38:41 +0100

    Update capitalisation in .onLoad

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,8 @@
 #Source python functions on load
 #' @export
 .onLoad <- function(libname, pkgname) {
-  reticulate::source_python(system.file("./python/init.py",package="ARTEMIS"),envir=globalenv())
-  reticulate::source_python(system.file("./python/score.py",package="ARTEMIS"),envir=globalenv())
-  reticulate::source_python(system.file("./python/align.py",package="ARTEMIS"),envir=globalenv())
-  reticulate::source_python(system.file("./python/main.py",package="ARTEMIS"),envir=globalenv())
+  reticulate::source_python(system.file("./Python/init.py",package="ARTEMIS"),envir=globalenv())
+  reticulate::source_python(system.file("./Python/score.py",package="ARTEMIS"),envir=globalenv())
+  reticulate::source_python(system.file("./Python/align.py",package="ARTEMIS"),envir=globalenv())
+  reticulate::source_python(system.file("./Python/main.py",package="ARTEMIS"),envir=globalenv())
 }
